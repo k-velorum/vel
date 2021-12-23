2b2249ec5e3e9f460401ed3e94639c982a5d9b7d
k-velorum <miyamoto@pinebase.jp>
2021-12-23 11:40:07 +0900

    ,抜け修正

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='VelLib',
     version='0.3.0',
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=['requests']
 )
